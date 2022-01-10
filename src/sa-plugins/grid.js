6fe6e525fdd5f92f0aeeb179e6fadb28d67dcaff
Johan Roxendal <johan.roxendal@litteraturbanken.se>
2022-01-10 15:37:48 +0100

    tweaked grid, added link.

--- src/sa-plugins/grid.js
+++ src/sa-plugins/grid.js
@@ -11,22 +11,21 @@ export default class Grid {
 	}
 
 	constructor({ data, config, api, readOnly }) {
-		console.log('🚀 ~ file: grid.js ~ line 14 ~ constructor');
 		this.data = data;
 		this.api = api;
 		this.config = config;
-		this.images = [];
-		for (const obj of data) {
-			this.images.push(new Image({ data: obj || {}, config, api, readOnly }));
+		this.items = [];
+		if (data.length > 0) {
+			for (const obj of data || []) {
+				this.items.push(this.renderItem(new Image({ data: obj || {}, config, api, readOnly }), obj.text, obj.link));
+			}
+		} else {
+			this.items.push(this.renderItem(new Image({ data: {}, config, api, readOnly })));
 		}
 	}
 
 	render() {
-		console.log('grid render');
-		// let input = $('<input>');
-		// input.val(this.data?.url || '');
-		// return input.get(0);
-		let wrapper = $(String.raw`<div class="grid">
+		let wrapper = $(String.raw`<div class="grid len-${this.items.length}">
             <div class="items"></div>
             <div class="buttons">
                 <button class="minus"><svg class="" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg></button>
@@ -34,25 +33,53 @@ export default class Grid {
             </div>
         </div>`);
 
+		let resetClass = () => wrapper.attr('class', 'grid').attr('class', 'grid len-' + this.items.length);
 		$('.plus', wrapper).on('click', () => {
-			let img = new Image({ data: {}, config: this.config, api: this.api, readOnly: this.readOnly });
-			this.images.push(img);
-			$('.items', wrapper).append(img.render());
+			let item = this.renderItem(new Image({ data: {}, config: this.config, api: this.api, readonly: this.readOnly }));
+			$('.items', wrapper).append(item.element);
+			this.items.push(item);
+			resetClass();
 		});
 		$('.minus', wrapper).on('click', () => {
-			this.images.pop();
+			this.items.pop();
 			$('.items', wrapper).children().last().remove();
+			resetClass();
 		});
 
-		for (let image of this.images) {
-			$('.items', wrapper).append(image.render());
+		for (let { element } of this.items) {
+			$('.items', wrapper).append(element);
 		}
 
 		return wrapper.get(0);
 	}
 
+	renderItem(image, paragraphText, link) {
+		console.log('🚀 ~ file: grid.js ~ line 57 ~ link', link);
+		let img = image.render();
+		let itemWrapper = $(String.raw`<div class="img">
+                <button style="display: ${paragraphText ? 'none' : 'block'}">Lägg till text</button>
+                <p style="display: none;" class="${paragraphText ? 'visible' : ''}" contenteditable>${
+			paragraphText || ''
+		}</p>
+                <div contenteditable class="link border" placeholder="länk">${link || ''}</div>
+            </div>`).prepend(img);
+		let button = $('button', itemWrapper).on('click', (event) => {
+			event.stopPropagation();
+			$('button', itemWrapper).css({ opacity: 0, position: 'absolute' });
+			$('p', itemWrapper).addClass('visible').text('Lorem ipsum dolor sit amet.');
+		});
+
+		return { image, element: itemWrapper };
+	}
+
 	save(blockContent) {
-		let values = this.images.map((image) => image.save());
+		let values = this.items.map((item) => ({
+			...item.image.save(),
+			text: item.element.find('p').text(),
+			link: item.element.find('.link').text(),
+		}));
+		// let values = this.items.map((item) => item.image.save());
+		console.log('🚀 ~ file: grid.js ~ line 86 ~ save values', values);
 		return values;
 	}
 }
